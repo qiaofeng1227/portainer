411ab60a9216a7d85ca80ce2b1b6f01282b5dc83
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-03-13 13:39:32 +0200

    refactor(kube): system resources settings

--- app/react/kubernetes/ServicesView/ServicesDatatable/ServicesDatatable.tsx
+++ app/react/kubernetes/ServicesView/ServicesDatatable/ServicesDatatable.tsx
@@ -22,10 +22,10 @@ import { useMutationDeleteServices, useServices } from '../service';
 import { Service } from '../types';
 import { DefaultDatatableSettings } from '../../datatables/DefaultDatatableSettings';
 import { isSystemNamespace } from '../../namespaces/utils';
+import { SystemResourcesAlert } from '../../datatables/SystemResourcesAlert';
 
 import { columns } from './columns';
 import { createStore } from './datatable-store';
-import { ServicesDatatableDescription } from './ServicesDatatableDescription';
 
 const storageKey = 'k8sServicesDatatable';
 const settingsStore = createStore(storageKey);
@@ -63,14 +63,11 @@ export function ServicesDatatable() {
       )}
       renderTableSettings={() => (
         <TableSettingsMenu>
-          <DefaultDatatableSettings
-            settings={tableState}
-            hideShowSystemResources={!isAdmin}
-          />
+          <DefaultDatatableSettings settings={tableState} />
         </TableSettingsMenu>
       )}
       description={
-        <ServicesDatatableDescription
+        <SystemResourcesAlert
           showSystemResources={tableState.showSystemResources || !isAdmin}
         />
       }
