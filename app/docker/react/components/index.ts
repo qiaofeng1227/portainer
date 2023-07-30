4af3fdbf340ccd4cbaeb09c78cf896a31f0adfb7
Chaim Lev-Ari <chaim.levi-ari@portainer.io>
2023-07-30 11:04:22 +0200

    --wip-- [skip ci]

--- app/docker/react/components/index.ts
+++ app/docker/react/components/index.ts
@@ -21,6 +21,8 @@ import { EventsDatatable } from '@/react/docker/events/EventsDatatables';
 import { ConfigsDatatable } from '@/react/docker/configs/ListView/ConfigsDatatable';
 import { AgentHostBrowser } from '@/react/docker/host/BrowseView/AgentHostBrowser';
 import { AgentVolumeBrowser } from '@/react/docker/volumes/BrowseView/AgentVolumeBrowser';
+import { StacksDatatable } from '@/react/docker/stacks/ListView/StacksDatatable/StacksDatatable';
+import { withUserProvider } from '@/react/test-utils/withUserProvider';
 
 const ngModule = angular
   .module('portainer.docker.react.components', [])
@@ -120,6 +122,15 @@ const ngModule = angular
       'onGoToParent',
       'onRename',
       'relativePath',
+    ])
+  )
+  .component(
+    'reactDockerStacksDatatable',
+    r2a(withUIRouter(withUserProvider(StacksDatatable)), [
+      'dataset',
+      // 'isImageNotificationEnabled',
+      'onReload',
+      'onRemove',
     ])
   )
   .component('dockerEventsDatatable', r2a(EventsDatatable, ['dataset']));
